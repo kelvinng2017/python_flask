5782fd8e57465c0c15b90ccef5468752dfa9c10d
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-09-07 22:16:37 +0800

    renew

---
+++ test_flask.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import socket
+app = Flask(__name__)
+
+
+@app.route("/")
+def test():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    # can change to app.run(host=ip you need ,port="port youneed",debug=True)
+    # get computer name
+    # use computer name get ip
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    app.run(host=ip, port=8787, debug=True)
